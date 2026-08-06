9a10968ba16781a2314f0b79972f36edf2b8e97b
custodiark <custodiark@gmail.com>
2026-08-06 09:57:33 -0300

    Add files via upload

---
+++ custodio-site/config.js
@@ -0,0 +1,22 @@
+/*
+ * Central de contatos e informações editáveis.
+ * Mantenha campos ainda não definidos vazios. Assim, seus botões e telefones não aparecem.
+ * Ao alterar o domínio, atualize também robots.txt e sitemap.xml, que são arquivos estáticos.
+ * Ao alterar o domínio, atualize também robots.txt e sitemap.xml, que são arquivos estáticos.
+ */
+window.CUSTODIO_CONFIG = {
+  domain: "custodiogestao.com.br",
+  email: {
+    general: "contato@custodiogestao.com.br",
+    rafael: "rafael@custodiogestao.com.br",
+    gabriel: "gabriel@custodiogestao.com.br"
+  },
+  phone: "(71) 98314-3061",
+  whatsappUrl: "",
+  schedulingUrl: "",
+  location: "Feira de Santana, Bahia",
+  price: {
+    show: false,
+    value: "R$ 2.000"
+  }
+};
